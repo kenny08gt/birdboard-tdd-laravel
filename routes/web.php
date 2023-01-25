b37050347c08897d675a9b6e5929d6ee4e271c0b
Alan Hurtarte <alan.hurtarte@gmail.com>
2023-01-25 13:41:41 -0600

    Added basic test

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::post("/projects", function () {
+    //validate
+    //persist
+    \App\Models\Project::create(request(["title", "description"]));
+    //redirect
+});
+
+Route::get("/projects", function () {
+    $projects = \App\Models\Project::all();
+
+    return view("projects.index", compact("projects"));
 });
